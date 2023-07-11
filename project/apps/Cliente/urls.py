314ba714a0b1de9484d58165471c97b1a287f256
Ruffos1 <lando330055@gmail.com>
2023-07-11 18:20:11 -0300

    cambios finales (clase 21)

--- project/apps/Cliente/urls.py
+++ project/apps/Cliente/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 """Include llama urls de otras apps"""
-from .views import index, cliente
+from .views import index, cliente, crear_cliente, busqueda
 app_name="Cliente"
 urlpatterns = [
     path("", index, name="Home" ),
-    path("clientes_predeterminados/", cliente, name = "clientes"  )
+    path("clientes_predeterminados/", cliente, name = "clientes"  ),
+    path("crear/", crear_cliente, name="crear"),
+    path("search/", busqueda, name="search")
     
 ]
 "Cda funcion cread se le agregara una urls"
--- project/apps/Cliente/views.py
+++ project/apps/Cliente/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse
+from datetime import date
 from.models import Cliente, Pais
+from .forms import ClienteForm
 
 # Create your views here.
 
@@ -24,3 +27,28 @@ def cliente(request):
     
     "Esto sirve para guardar automaticamente datos en la base"
     return redirect("Cliente:Home")
+
+"Funcion para crear forms"
+def crear_cliente(request : HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = ClienteForm(request.POST)
+        #Indiaca que esta variable debe guardarse
+        if form.is_valid():
+            form.save()
+            return redirect("Cliente:Home")
+    else: #request.method == "GET"
+        form = ClienteForm()
+
+    return render(request, "cliente/crear.html", context= {"form": form})
+
+def busqueda(request : HttpRequest) -> HttpResponse:
+    #Busqueda por nombre
+    cliente_nombre = Cliente.objects.filter(nombre__contains = "Matias")
+    #Buscar los que tienen un pais vacio
+    cliente_pais = Cliente.objects.filter(pais_origen_id=None)
+    #Busqueda por nacimiento (mayor a los 2000)
+    cliente_nacimiento = Cliente.objects.filter(nacimiento__gt = date(2000,1,1))
+    contexto ={"cliente_nombre":cliente_nombre,
+        "cliente_nacimiento":cliente_nacimiento,
+        "sin_pais": cliente_pais}
+    return render(request, "cliente/search.html", contexto)
